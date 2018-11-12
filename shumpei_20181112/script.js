77333343c8ccab66e66e74d58dc27a67ddf82225
shumpei0407 <taguchi@lifeistech.co.jp>
2018-11-13 00:46:31 +0900

    20111112
    music のカセットテープ部分追加

--- shumpei_20181112/script.js
+++ shumpei_20181112/script.js
@@ -7,18 +7,35 @@ function setup() {
   //noStroke();
 
   // 線の色を変える
-  stroke(random(mouseX),random(mouseX),random(mouseY) );
+  //stroke(random(255),random(255),random(255) );
     
 }
 
 function draw() {
   // 背景の色
-  background(255);
+  background(0);
   //background(255,255,255,0.2);
     
-  // 背景の動的な円
-  //fill(random(255),random(255),random(255));
-  //ellipse(width/ 2, height /2, random(1000));
+  //背景の円
+  //fill(0,0,0);
+  //テープの白穴
+  fill(255);
+  ellipse(width/ 4, height /2, 100);
+  ellipse(width* 3/4, height /2, 100);
+    
+  //テープの黒穴  
+  fill(0);
+  ellipse(width/ 4, height /2, 50);
+  ellipse(width* 3/4, height /2, 50);
+    
+  if (mouseIsPressed) {
+    
+　// ランダムに色を変える
+    fill(random(255), 255, random(255));
+    
+　// 円をマウスの位置にランダムな大きさで描く
+    ellipse(width/ 4, height /2, random(700));
+  }
     
   if (mouseIsPressed) {
     
@@ -26,40 +43,42 @@ function draw() {
     fill(255, random(255), random(255));
     
     // 円をマウスの位置にランダムな大きさで描く
-    ellipse(width/ 4, height /2, random(1000));
-  }
-    
-  if (mouseIsPressed) {
-    
-    // ランダムに色を変える
-    fill(random(255), 255, random(255));
-    
-    // 円をマウスの位置にランダムな大きさで描く
-    ellipse(width * 3/4 , height /2, random(1000));
+    ellipse(width * 3/4 , height /2, random(700));
   }
   
   // 文字の色
-  fill(0);
+  fill(255);
   
   // 文字の大きさ
   textSize(200);
   
   // フォント
-  textFont('Banana Yeti');
+  textFont('Snowboarding');
   
   // 文字を真ん中に表示
-  text('No Music', width/ 3, height /2);
+  text('No  Music', width/ 3, height /2);
+    
+  // 文字を真ん中に表示
+  text('No  Life :)', width/ 3.2, height /1.5);
+    
+  // テープ右上
+  text('┌', width/ 100, height /5);
+
+  // テープ左上
+  text('┐', width * 85/100, height /5);
+    
+  // テープ右下
+  text('└', width/ 100, height * 98/100);
+
+  // テープ左上
+  text('┘', width * 85/100, height * 98/100);
 
 　// 文字の大きさ
-  textSize(100);
+  textSize(200);
   
 　// フォント
-  textFont('Banana Yeti');
+  textFont('Snowboarding');
   
-  // 文字を真ん中に表示
-  text('No Life', width/ 3.5, height /1.5);
-    
-   // - - - - - - マウスが押されたら
     
 }
 
