fc8a1d21e91f992bcfee3ff2ed785067def3df4b
shumpei0407 <taguchi@lifeistech.co.jp>
2019-01-12 00:53:42 +0900

    20190111_2
    snow

--- shumpei_20190111/script.js
+++ shumpei_20190111/script.js
@@ -1,56 +1,50 @@
-var bubbles = [];//holds bubble objects
-var bubbleNum = 20;//# of bubbles?
-var xVariation = 2;//how much the bubbles move in the x direction
+var snow = [];
+var snowNum = 50;
+var xVariation = 0.5;
 
-//setup the canvas based on the window size
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   noStroke();
-  //frameRate(10);
-  //make random bubble objects {x,y,size,speed}
-  for (var i = 0; i < bubbleNum; i++) {
-    bubbles[i] = {
+  for (var i = 0; i < snowNum; i++) {
+    snow[i] = {
       x: random(window.innerWidth),
       y: random(window.innerHeight),
-      size: random(3, 10),
-      speed: 0
+      size: random(1, 30),
+      speed: 0,
     };
   }
-  //set color of the bubbles 'feel free to play with opacity, I think .4 looks nice'
-  fill("rgba(255,255,255, 0.4)");
+  fill("rgba(255,255,255)");
 }
-//handle window resize events and fix the canvas
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
-//animation loop
+
 function draw() {
-  background("#164899");
-  bubbles.forEach(function(bub, index) {
-    moveBubbles(bub);
-    if (bub.y < -10) newBubble(index);
+  background(28,48,91);
+  snow.forEach(function(snow, index) {
+    movesnow(snow);
+    if (snow.y > innerHeight) newsnow(index);
   });
 }
-//makes a new bubble at a specified index once its off screen
-function newBubble(index) {
-  bubbles[index] = {
+
+function newsnow(index) {
+  snow[index] = {
     x: random(window.innerWidth),
-    y: window.innerHeight,
-    size: random(3, 10),
-    speed: 0
+    y: 0,
+    size: random(1, 30),
+    speed: 0,
   };
 }
 
-//move the bubbles based on their size
-function moveBubbles(bubble) {
-  bubble.x += random(-xVariation, xVariation);
-  bubble.speed += bubble.size / 100;
-  bubble.y -= bubble.speed;
-  //draw the bubble NOTE they must be integer values to draw to the canvas
+function movesnow(snow) {
+  snow.x += random(-xVariation, xVariation);
+  snow.speed += snow.size / 1000;
+  snow.y += snow.speed;
   ellipse(
-    Math.round(bubble.x),
-    Math.round(bubble.y),
-    Math.round(bubble.size),
-    Math.round(bubble.size)
+    Math.round(snow.x),
+    Math.round(snow.y),
+    Math.round(snow.size),
+    Math.round(snow.size)
   );
 }
