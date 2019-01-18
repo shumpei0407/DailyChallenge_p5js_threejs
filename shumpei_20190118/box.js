0601913f4f95c384fa920b4edb2cd7dfa71b2feb
shumpei0407 <taguchi@lifeistech.co.jp>
2019-01-19 01:25:46 +0900

    20190118
    ‪Introduction to Matter.js （The Nature of Code）2

--- shumpei_20190118/box.js
+++ shumpei_20190118/box.js
@@ -1,14 +1,23 @@
 function Box(x,y,w,h){
-    this.body = Bodies.rectangle(x,y,w,h);
+    var options = {
+     friction:1,
+     restitution:0
+    }
+    this.body = Bodies.rectangle(x,y,w,h,options);
     this.w=w;
     this.h=h;
     World.add(world,this.body);
     this.show = function(){
         var pos = this.body.position;
         var angle = this.body.angle;
-        
     push();
     translate(pos.x,pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    noStroke();
+    //strokeWeight(3);
+    //stroke(random(200,255));
+    fill(random(200,255),random(200,255),255); 
     rect(0,0,this.w,this.h);
     pop();
     }
--- shumpei_20190118/sketch.js
+++ shumpei_20190118/sketch.js
@@ -8,22 +8,31 @@ var Bodies = Matter.Bodies;
 var engine;
 var world;
 var boxes = [];
+var ground;
 
 function setup(){
-    createCanvas(400,400);
+    createCanvas(400, 400);
     engine = Engine.create();
     world = engine.world;
     Engine.run(engine);
+    var options = {
+     isStatic: true
+    }
+    ground = Bodies.rectangle(200,height,width,10,options);
+    World.add(world,ground);
     
 }
 
-function mousePressed(){
-    boxes.push(new Box(mouseX,mouseY,20,20));
+function mouseDragged(){
+    boxes.push(new Box(mouseX,mouseY,random(20,80),(10,50)));
 }
 
 function draw(){
-    background(0);
+    background(25);
     for(var i = 0; i < boxes.length; i++){
          boxes[i].show();   
     }
+    fill(255);
+    strokeWeight(4);
+    rect(ground.position.x,ground.position.y,width,10);
 }
