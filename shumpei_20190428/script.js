12e7bb30699d55adb76b5a451a245687130f958a
shumpei0407 <40060447+shumpei0407@users.noreply.github.com>
2019-04-29 01:01:58 +0900

    20190428_2
    Rainbow

--- shumpei_20190428/script.js
+++ shumpei_20190428/script.js
@@ -11,22 +11,26 @@ function setup() {
 
 function draw() {
   push();
-    translate(width / 2, height / 2);
+    translate(width/2, height/2);
     rotate(angle);
-    scale(sizeRatio * tan(phaseScale))*cos(phaseScale);
+    scale(sizeRatio * cos(phaseScale));
     colorMode(RGB, 255, 255, 255, 1);
-    strokeWeight(0.1);
-    stroke(255, 255, 255, 10);
+    strokeWeight(0.001);
+    stroke(255, 255, 255, 1);
     colorMode(HSB, 360, 100, 100, 1);
     let c = color(phaseColor, 200, 200, 0.05);
     fill(c);
-    blendMode(ADD);
+    blendMode(HARD_LIGHT);
     //rect(displayWidth,0,100,100);
-    ellipse(100, 100, 1, 1000);
+    //ellipse(100, 100, 5, 5);
+    //ellipse(100, 100, 10, 10);
+    //ellipse(100, 100, 15, 15);
+    //ellipse(100, 100, 30, 30);
+    ellipse(0, 10, 200, 350);
   pop();
   
   angle %= TWO_PI;
-  angle += 0.1;
+  angle += 2;
   
   phaseScale %= TWO_PI;
   phaseScale += 0.001;
