a98a14f9eb7e4b2f072800185765465285d53301
shumpei0407 <40060447+shumpei0407@users.noreply.github.com>
2019-09-14 01:01:47 +0900

    201909132
    circle

--- shumpei_20190913/script.js
+++ shumpei_20190913/script.js
@@ -3,27 +3,28 @@ var pg2;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  colorMode(HSB, 360, 100, 100, 100);
+//  colorMode(HSB,360,100,100,100);   
+  colorMode(RGB, 255,255,255);
     
   pg = createGraphics(windowWidth, windowHeight);
   img = createImage(windowWidth, windowHeight);
   
   pg2 = createGraphics(windowWidth, windowHeight);
 
-  pg2.background(20,0,0);
-  pg2.stroke(255, 204, 0);
+  pg2.background(0);
+  pg2.stroke(255);
 	
-  for(var i = 0; i < width + 10; i+=15){
+  for(var i = 0; i < width + 10; i+=20){
     pg2.strokeWeight(i/40)
-    pg2.line(i,0,i,1000);
+    pg2.line(0,i,1000,i);
   }
 }
 
 function draw() {
-  background(0);
-  pg.background(255);
-  pg.fill(30);
-  pg.ellipse(mouseX,mouseY,200);
+  background(0,0,0,50);
+  pg.background(0);
+  pg.fill(255);
+  pg.ellipse(mouseX,mouseY,450);
   var maskedImage = pgMask(pg2, pg);
   image(maskedImage, 0, 0);
     
@@ -34,7 +35,7 @@ function draw() {
   textSize(22);
   text('NUMBER', 25, 40);
   textSize(50);
-  text('170', 25, 100);
+  text('171', 25, 100);
   textSize(22);
   textAlign(RIGHT);
   text('@shumpei0407', width - 50, height - 50);
