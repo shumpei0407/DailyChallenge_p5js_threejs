df305883a83c3a4a40352d22c77164e388e81bf4
shumpei0407 <40060447+shumpei0407@users.noreply.github.com>
2019-10-10 00:37:29 +0900

    20191009_2
    Trapezoid

--- shumpei_20191009/script.js
+++ shumpei_20191009/script.js
@@ -7,27 +7,26 @@ let state = false;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  for (let angle = 0; angle < 360; angle += 10) {
-    let v = p5.Vector.fromAngle(radians(angle - 135));
+  for (let angle = 0; angle < 360; angle += 2) {
+    let v = p5.Vector.fromAngle(radians(angle - 1));
     v.mult(150);
     circle.push(v);
     morph.push(createVector());
   }
 
-  for (let x = -100; x < 100; x += 1) {
+  for (let x = -100; x < 100; x += 20) {
     square.push(createVector(x, -100));
   }
   // Right side
-  for (let y = -100; y < 100; y += 1) {
-    square.push(createVector(10, y));
+  for (let y = -100; y < 100; y += 8) {
+    square.push(createVector(100, y));
   }
   // Bottom
-  for (let x = 100; x > -100; x -= 5) {
-    square.push(createVector(x, 100));
+  for (let x = 100; x > -100; x -= 1) {
     square.push(createVector(x, 100));
   }
   // Left side
-  for (let y = 100; y > -100; y -= 5) {
+  for (let y = 100; y > -100; y -= 1) {
     square.push(createVector(0, y));
   }
 }
@@ -53,22 +52,22 @@ function draw() {
       v1 = square[i];
     } else {
       v1 = circle[i];
-      v1 = circle[i];
     }
     let v2 = morph[i];
-    v2.lerp(v1, 0.1);
+    v2.lerp(v1, 0.09);
     totalDistance += p5.Vector.dist(v1, v2);
   }
 
-  if (totalDistance < 0.05) {
+  if (totalDistance < 0.01) {
     state = !state;
   }
 
+//  blendMode(SCREEN);
   translate(width / 2, height / 2);
-  rotate(frameCount/2);
+  rotate(frameCount);
   strokeWeight(10);
   beginShape();
-  fill(100);
+  fill(0);
   stroke(255);
   morph.forEach(v => {
     vertex(v.x, v.y);
