27860540d57cf86ff65a57b49aaa75a2be01bfb8
shumpei0407 <40060447+shumpei0407@users.noreply.github.com>
2019-11-15 00:48:34 +0900

    20191114_2
    hide

--- shumpei_20191114/script.js
+++ shumpei_20191114/script.js
@@ -7,74 +7,39 @@ var col3;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   
-  col1 = color(65, 105, 225);
-  col2 = color(15, 24, 51);
-  col3 = color(180, 191, 224);
+  col1 = color('#8EDAE5');
+  col2 = color('#F99695');
   
   console.log(col1);
 }
 
 function draw() {
-  background(255,10);
-  circleSize = map(mouseX, 0, width, 25, 100);
+  background(255);
+  circleSize = map(mouseX, 0, width, 2, 100);
   noFill(); 
+  noStroke();
   for (var x = 0; x <= width; x += spacing) {
-    for (var y = 0; y <= height; y += spacing) {
-      stroke(col1);
-      ellipse(x, y, circleSize+50, circleSize + 50); 
-      stroke(col2);
-      ellipse(x, y, circleSize+40, circleSize + 40);
-      stroke(col3);
-      ellipse(x, y, circleSize+30, circleSize + 30);
+    for (var y = 0; y <= height; y += spacing) {    
+      fill(col1);
+      rect(x, y, circleSize + 50, circleSize + 50); 
     }
   }
+
+  fill(col1);
+  ellipse(width/2,height/2,500,500);
+  fill(col2);
+  ellipse(100+width/2,height/2,500,500);
+
+  fill(col2);
+  strokeWeight(1);
+  textFont('Oswald');
+  textAlign(LEFT);
+  textSize(22);
+  text('NUMBER', 25, 40);
+  textSize(50);
+  text('204', 25, 100);
+  textSize(22);
+  textAlign(RIGHT);
+  text('@shumpei0407', width - 50, height - 50);
 }
 
-function mousePressed() {
-  col1 = color(random(255), 100, 0);
-  col2 = color(100, random(255), 0);
-  col3 = color(100, 0, random(255));
-}
-
-//
-//function setup() {
-//  createCanvas(windowWidth, windowHeight);
-//　background(25,25,200,100);
-//}
-//
-//function draw() {
-//  fill(255);
-//  strokeWeight(1);
-//  textFont('Oswald');
-//  textAlign(LEFT);
-//  textSize(22);
-//  text('NUMBER', 25, 40);
-//  textSize(50);
-//  text('203', 25, 100);
-//  textSize(22);
-//  textAlign(RIGHT);
-//  text('@shumpei0407', width - 50, height - 50);
-//    
-////  blendMode(SUBTRACT);
-//  fill(255,5);
-//  stroke(255);
-//  strokeWeight(0.5);
-//  ellipse(width/2, height/2, 400, 400)
-//    
-//  noStroke();
-//  fill(25,200,250,random(10));
-//  ellipse(random(80)+width/2, random(80)+height/2, 1000, 1000);
-//  fill(20,20,200,5);
-//  ellipse(random(80)+width/2, random(80)+height/2, 800, 800);
-//  fill(255,1);
-//  ellipse(random(80)+width/2, random(80)+height/2, 600, 600);
-//  fill(20,20,20,5);
-//  ellipse(random(80)+width/2, random(80)+height/2, 500, 500);
-//  fill(255,20,200,10);
-//  ellipse(random(80)+width/2, random(80)+height/2, 400, 400);
-////  fill(20,20,255,10);
-////  fill(10,10,255,10);
-////  ellipse(random(50)+width/2, random(50)+height/2, 300, 300);
-//  
-//
-//}
