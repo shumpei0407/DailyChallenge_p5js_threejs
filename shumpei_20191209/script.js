2bbd5896439ec5fb63fffaf0ac1846b894358903
shumpei0407 <40060447+shumpei0407@users.noreply.github.com>
2019-12-10 02:23:21 +0900

    20191209
    cafe

--- shumpei_20191209/script.js
+++ shumpei_20191209/script.js
@@ -5,10 +5,11 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   wall = loadImage('wall.jpg');
   colorMode(HSB, 360, 100, 100, 100);
+//　textStyle(BOLDITALIC);
   textFont('Yellowtail');
   strokeJoin(ROUND);
   rectMode(CENTER);
-  frameRate(200);
+  frameRate(400);
 
 }
 
@@ -16,7 +17,7 @@ function draw() {
     
   blendMode(BLEND);
   background(20);
-  tint(10,10,10,90);
+  tint(10,10,10,200);
   image(wall, 0, 0, width, height);
 
   if(random(1) < 0.5){
@@ -32,7 +33,7 @@ function draw() {
   for (var i = 1; i < 80; ++i) {
     strokeWeight(i*0.6);
     stroke(
-      map(i, 1, 50, 180, 260), 
+      map(i, 1, 50, 180, random(10,360)), 
       80, 
       map(i, 1, 50, 15, 1), 
       alp
@@ -40,12 +41,6 @@ function draw() {
     
 //    ellipse(width/2, height/2, 500, 500, 1);
 //    ellipse(width/2, height/2, width*0.9, height*0.9, 10);
-    textSize(150);
-    textAlign(CENTER, CENTER);
-//    text("Cafe", width/2.1, height/4*2.8);
-//    text("Cafe", width/2*1.01, height/3*2*1.01);
-    textSize(160);
-    textAlign(CENTER, CENTER);
       
     textAlign(LEFT);
     textSize(30);
@@ -56,38 +51,43 @@ function draw() {
     textSize(50);
     textAlign(RIGHT);
     text('@shumpei0407', width - 50, height - 50); 
+      
+    textSize(80);
+    textAlign(CENTER, CENTER);
+//    text("coffee", width/1.9, height/1.9);
+    text("COFFEE", width/2.05, height/2);
+    textSize(160);
+    textAlign(CENTER, CENTER);
+
   }
 
     for (var i = 1; i < 80; ++i) {
-    strokeWeight(i*0.6);
+    strokeWeight(i*0.7);
     stroke(
-      map(i, 1, 50, 180, 160), 
+      map(i, 1, 50, random(10,180), 360), 
       80, 
-      map(i, 1, 50, 15, 1), 
+      map(i, 1, 50, 15, 2), 
       alp
       );
     
-    ellipse(width/2, height/3, 400, 400, 1);
-    textSize(160);
+    ellipse(width/2, height/2, random(400,500));
+    textSize(100);
     textAlign(CENTER, CENTER);
-//    text("SHUMPEI`s", width/2, height/2.1);
-//    text("SHUMPEI`s", width/2, height/2.1);
+
   }
     
 
     for (var i = 1; i < 100; ++i) {
-    strokeWeight(i*1.2);
+    strokeWeight(i*0.8);
     stroke(
-      map(i, 1, 50, 180, 360), 
+      map(i, 1, 50, 180, 260), 
       80, 
       map(i, 1, 50, 15, 1), 
       alp
       );
     
-    textSize(160);
-    textAlign(CENTER, CENTER);
-//    text("SHUMPEI`s", width/2, height/2.1);
-//    text("SHUMPEI`s", width/1.98, height/2.08);
+    ellipse(width/2, height/2, 450);
+//    ellipse(width/2, height/2, 600);
   }
     
 }
