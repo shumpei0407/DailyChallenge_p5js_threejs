808e2cf202bc487b5987abb4cb1ed3dd5473f6be
shumpei0407 <40060447+shumpei0407@users.noreply.github.com>
2020-01-21 00:38:07 +0900

    20200120_2
    Japanese pattern ori

--- shumpei_20200120/sketch.js
+++ shumpei_20200120/sketch.js
@@ -1,77 +1,39 @@
+let i;
+
 function setup() {
   createCanvas(800, 800);
 }
 
 function draw() {
-  background(0);
+  background(30);
   noStroke();
-  for (let i = 0; i < height; i += 20) {
-    fill(200);
-    rect(0, i, width, 10);
+  for (let i = 0; i < height; i += 45) {
+    fill(217, 51, 63);
+    rect(0, i, width, 25);
     fill(255);
-    rect(i, 0, 10, height);
+    rect(i, 0, 25, height);
   }
+  noFill();
+  stroke(0);
+  strokeWeight(300);
+  ellipse(400,400,850,850);
+    
+    
+  textFont('Oswald');
+  fill(255);
+  strokeWeight(1);
+  stroke(255);
+  textAlign(LEFT);
+  textSize(15);
+  text('NUMBER', 50, 50);
+  textSize(30);
+  text('243', 52, 80);
+  textSize(15);
+  textAlign(RIGHT);
+  text('@shumpei0407', width - 50, height - 50); 
+  textAlign(CENTER);
+  textSize(20);
+  text('JAPANESE PATTERN', width/2, 720);
 }
 
 
-//
-//
-//let img;
-//let z = 1;
-//
-//function preload() {
-//  img = loadImage('iland.jpg');
-//}
-//
-//function setup() {
-//	createCanvas(800, 800);
-//	angleMode(DEGREES);
-//
-//}
-//
-//function draw() {
-//    blendMode(BLEND);
-//    noFill();
-//
-//	let cx = width / 2;
-//	let cy = height / 2;
-//
-//	let r = width / 2 * 0.7;
-//    
-//	push();
-//	translate(cx, cy);
-//	rotate(90);
-//	let sep = 15;
-//	let ox = -r + r / sep / 4;
-//	for (let x = ox; x < r; x += r / sep ) {
-//		let y = sqrt(sq(r) - sq(x));
-//        strokeWeight(z);
-//        stroke(217, 51, 63,z*3);
-//        z += 3;
-//        line(x, y, x, -y);
-////        line(x+y, y, -x+y, y);
-//	}
-//	pop();
-//	noLoop();
-//    
-//        stroke(0);
-//        strokeWeight(500);
-//        ellipse(400,400,1050,1050);
-//    
-//        textFont('Oswald');
-//        fill(255);
-//        strokeWeight(1);
-//        stroke(100);
-//        textAlign(LEFT);
-//        textSize(15);
-//        text('NUMBER', 50, 50);
-//        textSize(30);
-//        text('242', 52, 80);
-//        textSize(15);
-//        textAlign(RIGHT);
-//        text('@shumpei0407', width - 50, height - 50); 
-//        textAlign(CENTER);
-//        textSize(20);
-//      text('JAPANESE PATTERN', width/2, 700);
-//}
-//
