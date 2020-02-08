39d8a5a69e73db0b882509416247397a941e0745
shumpei0407 <40060447+shumpei0407@users.noreply.github.com>
2020-02-09 00:46:01 +0900

    20200208_2
    Sound Visualize‬

--- shumpei_20200208/sketch.js
+++ shumpei_20200208/sketch.js
@@ -8,45 +8,51 @@ function setup() {
   createCanvas(800, 800);
   song.loop();
 
-  // create a new Amplitude analyzer
   analyzer = new p5.Amplitude();
 
-  // Patch the input to an volume analyzer
   analyzer.setInput(song);
 }
 
 function draw() {
-//blendMode(EXCLUSION);
-  background(87,209,201,10);
+  background(87,209,201,5);
 
-  // Get the average (root mean square) amplitude
   let rms = analyzer.getLevel();
     
   fill(31,110,212,10);
   strokeWeight(100);
   stroke(31,110,212,10);
 
-  // Draw an ellipse with size based on volume
   ellipse(width / 2, height / 2, 10 + rms * 1500, 10 + rms * 1500);
     
     
     
-  fill(237,84,133,100);
+  fill(9,25,79,10);
   strokeWeight(50);
-  stroke(237,84,133,10);
+  stroke(9,25,79,10);
 
 
-  // Draw an ellipse with size based on volume
   ellipse(width / 2, height / 2, 10 + rms * 1000, 10 + rms * 1000);
     
     
 
-  fill(255,232,105,200);
+  fill(87,209,201,200);
   strokeWeight(1);
-  stroke(237,84,133,10);
+  stroke(87,209,201,10);
 
-  // Draw an ellipse with size based on volume
   ellipse(width / 2, height / 2, 10 + rms * 700, 10 + rms * 700);
+    
+  textFont('Oswald');
+  fill(255);
+  strokeWeight(1);
+  stroke(255);
+  textAlign(LEFT);
+  textSize(15);
+  text('NUMBER', 50, 50);
+  textSize(30);
+  text('250', 52, 80);
+  textSize(15);
+  textAlign(RIGHT);
+  text('@shumpei0407', width - 50, height - 50); 
 }
 
 
